212d7a6c1ee245bedca85f3dfa502c52b30f0a28
Erick <erickcassianopassos@gmail.com>
2023-07-15 21:31:16 -0300

    Implementada adição de vértices e de arestas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvasWidth = 400, canvasHeight = 400;
 const nodeRadius = 40;
-const mode = "MOVE"
+let mode = "MOVE";
+let nodeToAddEdgeFrom = -1;
 
 let nodes = []
 let edges = []
@@ -17,13 +18,6 @@ function setup() {
     for(let i = 0; i < genNodes; i++) {
         nodes.push(new Node(i));
     }
-
-    for(let i = 0; i < genNodes; i++) {
-        for(let j = i+1; j < genNodes; j++) {
-            if(i == j) continue;
-            edges.push(new Edge(i, j));
-        }
-    }
 }
 
 function draw() {
@@ -32,13 +26,67 @@ function draw() {
     nodes.forEach((node) => node.display())
 }
 
-function mousePressed() {
-    for(let i = nodes.length-1; i >= 0; i--) { // greater indexed nodes are displayed above
-        let d = dist(mouseX, mouseY, nodes[i].x, nodes[i].y);
-        if(d < nodeRadius) {
-            nodes[i].onClick();
+function keyPressed() {
+    if(keyCode === 65) {
+        mode = "NODE";
+        return;
+    }
+
+    if(keyCode === 69) {
+        mode = "EDGE";
+        return;
+    }
+}
+
+function keyReleased() {
+
+    switch (mode) {
+        case "EDGE":
+            resetNodeSelection();
             break;
-        }
+
+        default:
+            break;
+    }
+
+    mode = "MOVE";
+    
+    return;
+}
+
+function mousePressed() {
+
+    switch (mode) { // toma ações diferentes dependendo do MODO atual
+        case "NODE":
+            addNode(mouseX, mouseY);
+            break;
+
+        case "EDGE":
+            for(let i = nodes.length-1; i >= 0; i--) { // vertices de indices maiores estão acima
+                let d = dist(mouseX, mouseY, nodes[i].x, nodes[i].y);
+                if(d < nodeRadius) {
+                    if(nodeToAddEdgeFrom === -1) { // se não tem ninguém selecionado, selecione o vertice clicado
+                        nodes[i].select();
+                        nodeToAddEdgeFrom = i;
+                    } else {                        // se já tiver algum selecionado, adicione uma aresta entre eles e reseta
+                        addEdge(nodeToAddEdgeFrom, i);
+                        resetNodeSelection();
+                    }
+
+                    break;
+                }
+            }
+            break;
+            
+        default:
+            for(let i = nodes.length-1; i >= 0; i--) { // vertices de indices maiores estão acima
+                let d = dist(mouseX, mouseY, nodes[i].x, nodes[i].y);
+                if(d < nodeRadius) {
+                    nodes[i].onClick();
+                    break;
+                }
+            }
+            break;
     }
 }
 
@@ -48,6 +96,32 @@ function mouseReleased() {
             nodes[i].onRelease();
         }
     }
+}
+
+function resetNodeSelection() {
+    nodes.forEach(node => {
+        node.deselect();
+    });
+    nodeToAddEdgeFrom = -1;
+    return;
+}
+
+function addNode(x, y, label=nodes.length) {
+    nodes.forEach((node) => {
+        if(node.label === label) return;
+    });
+
+    nodes.push(new Node(label, x, y));
+    return;
+}
+
+function addEdge(source, destination, directed=false) {
+    edges.forEach(edge => {
+        if(edge.source === source && edge.destination === destination) return;
+        if(!directed && edge.source === edge.destination && edge.destination === edge.source) return; 
+    });
+
+    edges.push(new Edge(source, destination, directed));
 }
 
 class Edge {
@@ -83,6 +157,7 @@ class Node {
         this.x = x;
         this.y = y
         this.isMoving = false;
+        this.isSelected = false;
     }
 
     display() {
@@ -91,20 +166,29 @@ class Node {
             this.y=mouseY;
         }
 
+        push();
+        if(this.isSelected) strokeWeight(4);
         circle(this.x, this.y, nodeRadius);
+        pop();
         textAlign(CENTER, CENTER);
         text(this.label, this.x, this.y);
     }
 
+    select() {
+        this.isSelected = true;
+    } 
+
+    deselect() {
+        this.isSelected = false;
+    }
+
     onClick() {
         if(mode === "MOVE") {
-            console.log(`started moving node ${this.label}`);
             this.isMoving = true;
         }
     }
 
     onRelease() {
-        console.log(`stopped moving node ${this.label}`);
         if(mode === "MOVE") {
             this.isMoving = false;
         }
